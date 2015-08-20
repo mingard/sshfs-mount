979430141e6384fe2114165d6faf9ab32e29e0d0
System Administrator <root@Arthurs-MacBook-Pro-2.local>
2015-08-20 11:23:41 +0100

    Version 1.0.2

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sshfs-mount
 A quick way to mount and unmount volumes with SSHFS
+
+##Install
+1. Install [SSHFS and Fuse](http://osxfuse.github.io/)
+2. Create a config .mount file in the config folder, making sure that the local directory you specify exists and is writable
+
+#Run
+
+1. To mount, navigate to the root of the mount app. Run `. mount.sh mount {configFileName}`
+2. To unmount, do exactly the same, but substitute `mount` for `unmount`
+
+Special thanks to [@jean-luc](https://github.com/jean-luc) and [@nivlaps](https://github.com/nivlaps) for debugging
+
---
+++ mount.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for file in config/*
+do
+        if [ $file = 'config/'$2'.mount' ];
+        then
+            source $file
+            dir="$mountDir"'/'"$mountName"
+            if [ $1 = 'unmount' ];
+            then
+                umount "$dir"
+            elif [ $1 = 'mount' ];
+            then 
+                ssh-add "$pem"
+                mkdir "$dir"
+                sshfs -o reconnect "$username"@"$hostName":/"$remoteRootDir"  "$dir" -oauto_cache,reconnect,defer_permissions,negative_vncache,noappledouble,volname="$mountName"
+            fi
+        fi
+done
