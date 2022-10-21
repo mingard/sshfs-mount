561133d189e28de870415048a02bd854323acdd0
Arthur Mingard <info@arthurmingard.com>
2022-10-21 09:54:00 +0100

    feat: support for ports

--- mount.sh
+++ mount.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
-path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-for file in $path/config/*
+#!/bin/zsh
+
+# sysinfo_page - A script to mount a remote instance using SSHFS
+SOURCE=${(%):-%N}
+cpath=$( cd "$(dirname "${SOURCE}")" ; pwd -P )
+
+for file in $cpath/config/*
 do
-        if [ $file = $path'/config/'$2'.mount' ];
+        if [ $file = $cpath'/config/'$2'.mount' ];
         then
             source $file
             dir="$mountDir"'/'"$mountName"
+            if [ -z "${port}" ]; then 
+                PORT=22
+            else 
+                PORT=$port
+            fi
+                echo $PORT
             if [ $1 = 'unmount' ];
             then
                 umount "$dir"
@@ -13,8 +23,15 @@ do
             then 
                 ssh-add "$pem"
                 mkdir "$dir"
-                sshfs -o reconnect "$username"@"$hostName":/"$remoteRootDir"  "$dir" -oauto_cache,reconnect,defer_permissions,negative_vncache,noappledouble,volname="$mountName"
+                sshfs -o reconnect "$username"@"$hostName":/"$remoteRootDir" "$dir" -p $PORT -oauto_cache,reconnect,defer_permissions,negative_vncache,noappledouble,volname="$mountName"
                 echo "Connected to " $hostName
+            fi
+            if [ $3 ];
+            then
+                if [ $3 = "connect" ];
+                then
+                    ssh -p $PORT $username"@"$hostName
+                fi
             fi
         fi
 done
